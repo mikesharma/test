1e213d12a8ceec2338a00eca691ff307cc056a4a
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2018-08-08 17:21:45 -0400

    Merge Identity Entities if two claim the same alias
    Past bugs/race conditions meant two entities could be created each
    claiming the same alias. There are planned longer term fixes for this
    (outside of the race condition being fixed in 0.10.4) that involve
    changing the data model, but this is an immediate workaround that has
    the same net effect: if two entities claim the same alias, assume they
    were created due to this race condition and merge them.
    
    In this situation, also automatically merge policies so we don't lose
    e.g. RGPs.

--- helper/strutil/strutil_test.go
+++ helper/strutil/strutil_test.go
@@ -423,3 +423,13 @@ func TestStrUtil_RemoveDuplicates(t *testing.T) {
 		}
 	}
 }
+
+func TestStrUtil_MergeSlices(t *testing.T) {
+	res := MergeSlices([]string{"a", "c", "d"}, []string{}, []string{"c", "f", "a"}, nil, []string{"foo"})
+
+	expect := []string{"a", "c", "d", "f", "foo"}
+
+	if !reflect.DeepEqual(res, expect) {
+		t.Fatalf("expected %v, got %v", expect, res)
+	}
+}
